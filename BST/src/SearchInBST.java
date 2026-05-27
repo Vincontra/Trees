84b0e94ff6382a3218e110a53ba53af450666290
vinay <vinaygadge2003@gmail.com>
2026-05-27 12:27:05 +0530

    bst started

---
+++ BST/src/SearchInBST.java
@@ -0,0 +1,23 @@
+//import javax.swing.tree.TreeNode;
+//
+//public class SearchInBST {
+//  //  keep in mind that the  bst are invented for less tc so there height is logN for most of the time
+//  public static void main(String[] args) {
+//
+//  }
+//  public static boolean func(TreeNode root,int tar){
+//      if (root==null){
+//          return false;
+//      }
+//      if (root.val==tar){
+//          return true;
+//      }
+//      if(root.val>tar){
+//          return func(root.left,tar)
+//      }
+//      return func(root.right,tar);
+//  }
+//
+//
+//
+//}
