ad8d2bb8ee42110b48a7fbedb9e2547dd7e5ac1d
vinay <vinaygadge2003@gmail.com>
2026-05-27 18:22:59 +0530

    Insert IntoBST LC 701

--- BST/src/InsertInBST.java
+++ BST/src/InsertInBST.java
@@ -1,8 +1,8 @@
 public class InsertInBST {
     static class TreeNode{
         int val;
-        CeilAndFloor.TreeNode left;
-        CeilAndFloor.TreeNode right;
+        TreeNode left;
+        TreeNode right;
         public TreeNode(int val){
             this.val=val;
             this.left=null;
@@ -12,6 +12,19 @@ public class InsertInBST {
 
     public static void main(String[] args) {
 
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+            if(root==null){
+                TreeNode node=new TreeNode(val);
+                return node;
+            }
+            else if(root.val>val){
+                root.left=insertIntoBST(root.left,val);
+            }
+            else{
+                root.right=insertIntoBST(root.right,val);
+            }
+            return root;
     }
 
 
