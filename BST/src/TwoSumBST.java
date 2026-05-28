6dc92ebc1dad2dfba1c8922311db5d21ac1703ad
vinay <vinaygadge2003@gmail.com>
2026-05-28 19:34:06 +0530

    itreator ka dekh lena you will get it

---
+++ BST/src/TwoSumBST.java
@@ -0,0 +1,11 @@
+public class TwoSumBST {
+    // isme kuch nhi most optimized would be
+    // 2 alag alag baar BSTiterator laga do
+    // jaise next nikalte
+    // same ulta before
+    // and fir two 2 pointers ki tarah we can do that
+    // target se jyada hai to kam krna padega so before
+    // kam hai bahdhana padega to next
+    // not writng code as BST itr ka likha hai
+
+}
