e4b07b5d2eb2933914c9eb3e55c621675a15310b
vinay <vinaygadge2003@gmail.com>
2026-05-28 15:35:05 +0530

    Construction IMP

---
+++ BST/src/ConstructBSTFromPreorder.java
@@ -0,0 +1,26 @@
+public class ConstructBSTFromPreorder {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val){
+
+        }
+    }
+    public static void main(String[] args) {
+        // 8 5 1 7 10 12
+
+
+
+    }
+    public static TreeNode func(int arr[],int upBound,int i[]){
+        if (i[0]==arr.length||arr[i[0]]>upBound){
+            return null;
+        }
+        TreeNode node = new TreeNode(arr[i[0]]);
+        i[0]++;
+        node.left=func(arr, node.val,i);
+        node.right=func(arr,upBound,i);
+        return node;
+    }
+}
